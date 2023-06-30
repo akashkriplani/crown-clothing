4a9d7deb4382e1f8fb9ff53be79f8871b950bbf2
Akash Kriplani <aakash23kriplani@gmail.com>
2023-06-30 16:46:25 +0530

    Modify product-card component to use styled-components

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,4 +1,5 @@
 import ProductCard from '../product-card/product-card.component';
+
 import { CategoryPreviewContainer, Preview, Title } from './category-preview.styles';
 
 const CategoryPreview = ({ title, products }) => {
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import { CartContext } from '../../contexts/cart.context';
 
-import './product-card.styles.scss';
+import { Footer, Name, Price, ProductCardContainer } from './product-card.styles';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -11,16 +11,16 @@ const ProductCard = ({ product }) => {
   const addProductToCart = () => addItemToCart(product);
 
   return (
-    <div className="product-card-container">
+    <ProductCardContainer>
       <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
       <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>
         Add to cart
       </Button>
-    </div>
+    </ProductCardContainer>
   );
 };
 
