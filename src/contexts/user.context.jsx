e64ae85acf0518d19dfbc3c3a29a4a8518230c20
Akash Kriplani <aakash23kriplani@gmail.com>
2023-07-03 16:16:10 +0530

    Use useReducer inside cart context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useReducer } from 'react';
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from '../utils/firebase/firebase.utils';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 export const UserContext = createContext({
   currentUser: null,
@@ -20,7 +21,7 @@ const userReducer = (state, action) => {
         currentUser: payload
       };
     default:
-      throw new Error(`Unhandled type ${type} in userReducer`);
+      throw new Error(`Unhandled type of ${type} in userReducer`);
   }
 };
 
@@ -34,7 +35,7 @@ export const UserProvider = ({ children }) => {
   const { currentUser } = state;
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
