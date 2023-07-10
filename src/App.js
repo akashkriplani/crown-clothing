97d884bfc52300d64396edb1056090a3386526f8
Akash Kriplani <aakash23kriplani@gmail.com>
2023-07-10 16:40:32 +0530

    Convert onAuthStateChanged listener to promise

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from './utils/firebase/firebase.utils';
+import {
+  createUserDocumentFromAuth,
+  getCurrentUser,
+  onAuthStateChangedListener
+} from './utils/firebase/firebase.utils';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
 import Navigation from './routes/navigation/navigation.component';
@@ -14,15 +18,10 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
+    getCurrentUser().then((user) => {
+      console.log(user);
     });
-
-    return unsubscribe;
-  }, [dispatch]);
+  }, []);
 
   return (
     <Routes>
