2a6c659c8ac07034869089fce826f95510d8f928
Akash Kriplani <aakash23kriplani@gmail.com>
2023-07-21 17:00:03 +0530

    Convert navigation component to typescript

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -26,7 +26,7 @@ const Navigation = () => {
         <NavLinks>
           <NavLink to="/shop">SHOP</NavLink>
           {currentUser ? (
-            <NavLink as="span" onClick={signOutUser}>
+            <NavLink to="/" as="span" onClick={signOutUser}>
               SIGN OUT
             </NavLink>
           ) : (
