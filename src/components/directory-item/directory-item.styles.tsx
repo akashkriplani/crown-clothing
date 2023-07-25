6dc9237bf021426cfa5178eb0d43642996bc5a08
Akash Kriplani <aakash23kriplani@gmail.com>
2023-07-25 14:46:19 +0530

    Convert directory item component to typescript

--- src/components/directory-item/directory-item.styles.tsx
+++ src/components/directory-item/directory-item.styles.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
-export const BackgroundImage = styled.div.withConfig({
-  shouldForwardProp: (prop) => !['imageUrl'].includes(prop)
-})`
+type BackgroundImageProps = {
+  imageUrl: string;
+};
+
+export const BackgroundImage = styled.div<BackgroundImageProps>`
   width: 100%;
   height: 100%;
   background-size: cover;
