71c17bbe851e57d7dcdab2e81b5b2bf82a142c8a
Akash Kriplani <aakash23kriplani@gmail.com>
2023-07-25 14:41:11 +0530

    Convert checkout item component to typescript

--- src/components/checkout-item/checkout-item.component.tsx
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -1,6 +1,8 @@
+import { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItemToCart, removeItemFromCart, clearItemFromCart } from '../../store/cart/cart.action';
 import { selectCartItems } from '../../store/cart/cart.selector';
+import { CartItem } from '../../store/cart/cart.types';
 
 import {
   CheckoutItemContainer,
@@ -12,7 +14,11 @@ import {
   RemoveButton
 } from './checkout-item.styles';
 
-const CheckoutItem = ({ cartItem }) => {
+type CheckoutItemProps = {
+  cartItem: CartItem;
+};
+
+const CheckoutItem: FC<CheckoutItemProps> = ({ cartItem }) => {
   const { imageUrl, name, quantity, price } = cartItem;
 
   const dispatch = useDispatch();
