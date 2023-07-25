9cb35304878980a2f67470ccd35d4cc3d9fe682c
Akash Kriplani <aakash23kriplani@gmail.com>
2023-07-25 14:48:16 +0530

    Convert product card component to typescript

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,11 +1,17 @@
+import { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import { addItemToCart } from '../../store/cart/cart.action';
+import { CategoryItem } from '../../store/categories/category.types';
 
 import { Footer, Name, Price, ProductCardContainer } from './product-card.styles';
 import { selectCartItems } from '../../store/cart/cart.selector';
 
-const ProductCard = ({ product }) => {
+type ProductCardProps = {
+  product: CategoryItem;
+};
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
   const { name, price, imageUrl } = product;
   const dispatch = useDispatch();
 
