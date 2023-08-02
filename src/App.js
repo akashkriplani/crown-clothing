dff9e2ce62da9c0301b4eb4d89398a863abec4a1
Akash Kriplani <aakash23kriplani@gmail.com>
2023-08-02 16:16:19 +0530

    Include global styles in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router-dom';
 import Spinner from './components/spinner/spinner.component';
 import { checkUserSession } from './store/user/user.action';
 
+import { GlobalStyle } from './global.styles';
+
 const Authentication = lazy(() => import('./routes/authentication/authentication.component'));
 const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
 const Home = lazy(() => import('./routes/home/home.component'));
@@ -20,6 +22,7 @@ const App = () => {
 
   return (
     <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
